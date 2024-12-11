c3d5c61394d779c2f2385cfb65551eb28ee45689
David Olowomeye <olowomeyedavid242@gmail.com>
2024-12-11 14:01:32 +0000

    Implemented Push Notification

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -34,3 +34,47 @@ export const getNotification = async (
     });
   }
 };
+
+export const pushNotification = async (
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  try {
+    const userId = req.user?.id;
+    const { title, message, isRead } = req.body;
+
+    if (!title || !message || isRead) {
+      return res.status(400).json({
+        status: 400,
+        message: "Please fill all notification fields",
+      });
+    }
+
+    // const notificationData = {
+    //   userId,
+    //   title: "Push Notification Activated",
+    //   message:
+    //     "Your push notification feature has been successfully activated.",
+    //   isRead: false,
+    // };
+
+    const newNotification = await NotificationModel.create({
+      user: userId,
+      title,
+      message,
+      isRead,
+    });
+
+    return res.status(200).json({
+      status: 200,
+      message: "Push notification activated successfully",
+      data: newNotification,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: "An error occurred while activating the push notification",
+    });
+  }
+};
--- src/routes/notification.route.ts
+++ src/routes/notification.route.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectedRoute } from "../middleware/auth.middleware";
-import { getNotification } from "../controllers/notification.controller";
+import {
+  getNotification,
+  pushNotification,
+} from "../controllers/notification.controller";
 
 const router = express.Router();
 
 router.get("/notifications/:id", protectedRoute, getNotification);
+router.post("/notifications", protectedRoute, pushNotification);
 
 export default router;
